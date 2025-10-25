e91ff82862614b00e27e99d5486dea555d1db744
MGuerra50 <matheusguerra50@gmail.com>
2025-10-25 14:03:18 -0300

    feat: refatoração da tabela principal e adição da ultima seção da dashboard

--- src/app/components/LowStockTable/LowStockTable.tsx
+++ src/app/components/LowStockTable/LowStockTable.tsx
@@ -1,20 +1,56 @@
 "use client";
 
+import ButtonPrincipal from "../ButtonPrincipal/ButtonPrincipal";
 import TablePrincipal from "../TablePrincipal/TablePrincipal";
 import WidgetsBase from "../WidgetsBase/WidgetsBase";
 
 interface props {
   height?: number;
 }
+
+interface Product {
+  name: string;
+  quantity: number;
+  max: number;
+}
+
+const products: Product[] = [
+  { name: "Mouse gamer rgb", quantity: 5, max: 10 },
+  { name: "Mouse Attack shack", quantity: 1, max: 10 },
+  { name: "Teclado gamer", quantity: 7, max: 10 },
+  { name: "Mouse pad gamer", quantity: 8, max: 10 },
+];
+
+const productsColumns = [
+  { key: "name", label: "Produto" },
+  { key: "quantity", label: "Quantidade" },
+  { key: "max", label: "Máximo" },
+] satisfies { key: keyof Product; label: string }[];
+
 export default function LowStockTable({ height }: props) {
   return (
     <>
       <div className="h-full">
         <WidgetsBase>
-          <div style={height ? { height: height + "px" } : { height: "auto" }}>
-            <h2 className="text-lg font-semibold mb-3 pl-[5px]">Low Stock</h2>
-            <TablePrincipal />
-          </div>
+          <>
+            <div
+              style={height ? { height: height + "px" } : { height: "auto" }}
+            >
+              <h2 className="text-lg font-semibold mb-3 pl-[5px]">Low Stock</h2>
+              <TablePrincipal
+                rows={products}
+                columns={productsColumns}
+                renderActions={(product) => (
+                  <ButtonPrincipal width={90}>
+                    <div className="font-semibold mt-[15px] mb-[15px]">BUY</div>
+                  </ButtonPrincipal>
+                )}
+              />
+            </div>
+            <div className="w-full h-full flex justify-center mb-[10px] mt-[35px] text-lg font-semibold">
+              <ButtonPrincipal text="See more" />
+            </div>
+          </>
         </WidgetsBase>
       </div>
     </>
